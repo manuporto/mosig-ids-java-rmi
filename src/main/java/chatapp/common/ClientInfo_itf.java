ba61fb4e2372b7eaccb448341063bec1e61da0fe
Manuel Porto <manu.porto94@gmail.com>
2019-02-07 16:05:11 +0100

    Fix packages names

--- src/main/java/chatapp/common/ClientInfo_itf.java
+++ src/main/java/chatapp/common/ClientInfo_itf.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package server;
+package chatapp.common;
 
-/**
- *
- * @author admin
- */
 public interface ClientInfo_itf {
     
 }
--- src/main/java/chatapp/common/MessageService_impl.java
+++ src/main/java/chatapp/common/MessageService_impl.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package client;
+package chatapp.common;
 
-/**
- *
- * @author admin
- */
 public class MessageService_impl {
     
 }
