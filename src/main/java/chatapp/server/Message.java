fae087862dcfd536c44e4d4643012baebcf184dc
Manuel Porto <manu.porto94@gmail.com>
2019-02-11 20:32:21 +0100

    Declare classes for defining messages and storing them

---
+++ src/main/java/chatapp/server/Message.java
@@ -0,0 +1,32 @@
+package chatapp.server;
+
+import java.util.Date;
+
+public class Message {
+    private Date timeStamp;
+    private String author;
+    private String msg;
+
+    public Message(Date timeStamp, String author, String msg) {
+        this.timeStamp = timeStamp;
+        this.author = author;
+        this.msg = msg;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + timeStamp + "] " + author + ": " + msg;
+    }
+}
