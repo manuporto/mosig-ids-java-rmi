2c8dc361ea27a3190e810a6e5f5f61ba95460bd8
Manuel Porto <manu.porto94@gmail.com>
2019-02-16 10:45:19 +0100

    Close client properly

--- src/main/java/chatapp/client/Client.java
+++ src/main/java/chatapp/client/Client.java
@@ -8,6 +8,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -66,6 +67,7 @@ public class Client {
         accessService.leave(clientInfo);
         registry.unbind("AccessService");
         registry.unbind("MessageService");
+        UnicastRemoteObject.unexportObject(clientInfo, true);
         scanner.close();
     }
 }
